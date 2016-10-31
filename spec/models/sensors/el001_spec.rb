3e64b7f62d8314fb05cb6ac77d8caf9b7fc5c997
Adam Martak <adam.martak@railsformers.com>
2016-10-31 08:53:46 +0100

    fix spec

--- spec/models/sensors/el001_spec.rb
+++ spec/models/sensors/el001_spec.rb
@@ -5,9 +5,15 @@ describe Sensors::El001 do
   let(:message) { double(payloadHex: "210000000000001140") }
   let!(:sensor) { described_class.new(message: message) }
 
-  describe ".energy" do
-    subject { sensor.energy }
+  describe ".energy_minus" do
+    subject { sensor.energy_minus }
 
     it { expect(subject).to eq(44.16) }
+  end
+
+  describe ".energy_plus" do
+    subject { sensor.energy_plus }
+
+    it { expect(subject).to eq(0.0) }
   end
 end
