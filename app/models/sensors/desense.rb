1e10cb0d087542c4b76bd5936da913b9864cb1b4
Adam Martak <adam.martak@railsformers.com>
2016-11-03 20:59:51 +0100

    refactor

--- app/models/sensors/desense.rb
+++ app/models/sensors/desense.rb
@@ -3,10 +3,6 @@ module Sensors
     attributes :temperature, :humidity
     display_as :line_chart, group_by: :minute, attributes: [:temperature, :humidity]
 
-    def payloadHexa
-      message.payloadHex.gsub(/../) { |pair| pair.hex.chr }
-    end
-
     def temperature
       @temperature ||= values.temperature / 1000.0
     end
